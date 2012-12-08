db3e3dc61a8c5061681e55e89a09b3bb0e8eac0d
Yatin Sarbalia <yatinsns@gmail.com>
2012-12-08 12:53:45 +0530

    Initial Commit

---
+++ Ping/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ping
+//
+//  Created by Yatin Sarbalia on 08/12/12.
+//  Copyright (c) 2012 Yatin Sarbalia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ping/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ping
+//
+//  Created by Yatin Sarbalia on 08/12/12.
+//  Copyright (c) 2012 Yatin Sarbalia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
